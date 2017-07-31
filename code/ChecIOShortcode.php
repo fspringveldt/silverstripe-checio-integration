5ef56a62651f2af3a23e202d586f23684a0e808a
Franco Springveldt <franco@silverstripe.com>
2017-07-31 16:56:37 +1200

    FIX ChecIOShortcode extends from Object and not SiteTree

--- code/ChecIOShortcode.php
+++ code/ChecIOShortcode.php
@@ -1,11 +1,7 @@
 <?php
 
-class ChecIOShortcode extends SiteTree
+class ChecIOShortcode extends Object
 {
-    private static $casting = array(
-        'BuyNowButton' => 'HTMLText'
-    );
-
     /**
      * ShortCode which adds a link to a chec.io buy now popup window
      * @param $arguments
@@ -17,7 +13,7 @@ class ChecIOShortcode extends SiteTree
     public static function BuyNowButton($arguments, $content = null, $parser = null, $tagName)
     {
         if (isset($arguments['data-chec-product-id'])) {
-            $id = Convert::html2raw($arguments['data-chec-product-id']);
+            $id = Convert::raw2att($arguments['data-chec-product-id']);
             $siteURL = self::config()->get('third-party-url');
 
             // Add an onclick attribute to prevent following the re-written link anchor.
@@ -26,7 +22,7 @@ class ChecIOShortcode extends SiteTree
             $link = "<a href=\"$siteURL/{$id}\"";
             // Add all arguments as-is as we could potentially be receiving css class styles
             foreach ($arguments as $key => $val) {
-                $val = Convert::html2raw($val);
+                $val = Convert::raw2att($val);
                 $link .= " {$key}=\"{$val}\" ";
             }
             $link .= ">{$content}</a>";
